faa0ecd4a715849db11f99c6188aa5dca92d7902
lealinehocine <lea.line.saad@gmail.com>
2024-10-08 17:25:31 +0200

    TP2 - début de l'exo3

---
+++ TP1/SitePro/v2/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Léa-Line SAAD - Site Pro Accueil</title>
+<link rel="stylesheet" href="style1.css" type="text/css" media="screen" title="default" />
+</head>
+
+<body>
+    <h1>Léa-Line SAAD - Accueil</h1>
+
+    <div class="center">
+<nav class="menu">
+    <ul>
+    <li><a id="currentpage" href="index.html">Accueil</a></li>
+    <li><a href="cv.html">CV</a></li>
+    <li><a href="projets.html">Hobbies</a></li>
+    </ul>
+    </nav>
+
+<div class="description">
+<p>Ceci est le site pro de Léa-Line SAAD. </p>
+
+<p>Etudiante ingénieure en filière numérique à IMT Nord Europe,<br> je recherche un stage 
+    d’une durée de 4 mois  à compter du 12 mai 2025 
+    <br>dans le domaine du développement web et logiciel.
+    <br>Je suis ouverte à la mobilité internationale.</p>
+
+<!--Mettre un visuel-->
+</div>
+</div>
+<p class="bottom">© Léa-Line SAAD - Dernière mise à jour le 08/10/2024</p>
+
+</body>
---
+++ TP1/SitePro/v2/projets.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Léa-Line SAAD - Hobbies</title>
+<link rel="stylesheet" href="style1.css" type="text/css" media="screen" title="default" />
+</head>
+<body>
+    <h1>Léa-Line SAAD - Hobbies</h1>
+
+    <div class="center">
+<nav class="menu">
+    <ul>
+    <li><a href="index.html">Accueil</a></li>
+    <li><a href="cv.html">CV</a></li>
+    <li><a href="projets.html">Hobbies</a></li>
+    </ul>
+    </nav>
+    <div class="container">
+    <div class="rubrique"> 
+    <h1>Investissement associatif</h1>
+    <h3>Bureau Des Arts IMT Nord Europe - Année scolaire 2024-2025</h3>
+    <p>Responsable du pôle Maison Des Elèves</p>
+    </div>
+
+    <div class="rubrique"> 
+    <h1>Centres d'intérêt</h1>
+    <p>Guitare, chant, cinéma</p>
+    </div>
+
+    <div class="rubrique"> 
+    <h1>Sport</h1>
+    <p>Boxe anglaise, natation, course à pieds</p>
+    </div>
+
+</div>
+</div> 
+  <p class="bottom">© Léa-Line SAAD - Dernière mise à jour le 08/10/2024</p>
+
+</body>
