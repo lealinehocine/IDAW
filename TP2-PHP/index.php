bed76aa6c9fac04446908fbd2f72615b6f6b6148
lealinehocine <lea.line.saad@gmail.com>
2024-10-08 17:10:53 +0200

    TP 2 - exercice PHP donne l’heure

---
+++ TP2-PHP/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title> Exemple Dynamique </title>
+</head>
+<body>
+L'heure courante est :
+<?php
+// Affichage de la date et de l'heure  
+echo date('Y-m-d H:i:s');
+?>
+</body>
+</html>
+
