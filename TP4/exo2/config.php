4599102d8763b560537f030db66b9225c8c05579
lealinehocine <lea.line.saad@gmail.com>
2024-10-14 12:06:26 +0200

    TP4 2.2 à finir

---
+++ TP4/exo2/config.php
@@ -0,0 +1,10 @@
+<?php
+// phpinfo(); sert à vérifier que PDO est bien installé
+
+define('_MYSQL_HOST','127.0.0.1');
+define('_MYSQL_PORT',3306);
+define('_MYSQL_DBNAME','dbtest');
+define('_MYSQL_USER','root');
+define('_MYSQL_PASSWORD','pwd');
+
+?> 
---
+++ TP4/exo2/users.php
@@ -0,0 +1,21 @@
+<?php
+require_once('config.php');
+$connectionString = "mysql:host=". _MYSQL_HOST;
+if(defined('_MYSQL_PORT'))
+$connectionString .= ";port=". _MYSQL_PORT;
+$connectionString .= ";dbname=" . _MYSQL_DBNAME;
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
+$pdo = NULL;
+try {
+$pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $erreur) {
+echo 'Erreur : '.$erreur->getMessage();
+}
+$request = $pdo->prepare("select * from users");
+// TODO: add your code here
+// retrieve data from database using fetch(PDO::FETCH_OBJ) and
+// display them in HTML array
+/*** close the database connection ***/
+$pdo = null;
